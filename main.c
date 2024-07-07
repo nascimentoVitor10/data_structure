2ea61d8548fd14358032ea57acdf5a2d3692c57e
nascimentoVitor10 <vitorramosnascimento10@gmail.com>
2024-07-07 18:11:05 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,32 +1,69 @@
-#include "function.h"
-#include <stdio.h>
+int main(void) {
+  
+  char nome_paciente[100];
+  int ano, mes, dia;
+  int max_mes[] = {31,28, 30, 30, 31, 29, 31, 31, 30, 31, 30, 31}; // número máximo de dias de cada mês do ano
 
-int main(void)
-{
-    char str1[50] = "Hello, ";
-    char str2[] = "World!";
+  // leitura do nome completo do paciente
+  printf("Informe o nome do paciente: ");
+  fgets(nome_paciente, sizeof(nome_paciente), stdin);
+  nome_paciente[strcspn(nome_paciente, "\n")] = 0;
 
-    // my_strlen
-    int length = my_strlen(str1);
-    printf("Length of str1: %d\n", length);
+  // leitura do ano de nascimento do paciente
+  printf("Informe o ano de nascimento: ");
+  scanf("%d",&ano);
+  
+  // leitura do mês de nascimento do paciente
+  do {
+    printf("Informe o mês de nascimento: ");
+    scanf("%d",&mes);
+  } while (mes < 1 || mes > 12);
+  
+  // leitura do dia de nascimento do paciente
+  do {
+    printf("Informe o dia de nascimento: ");
+    scanf("%d",&dia);
+  } while (verify_day(ano, mes, dia, &max_mes) != 1);
+  printf("\n");
+    
+  // Definindo uma data de nascimento fictícia para o paciente
+  struct tm birthdate = {0}; // Inicializar a estrutura com zeros
+  birthdate.tm_year = ano - 1900;
+  birthdate.tm_mon = mes - 1; 
+  birthdate.tm_mday = dia; 
+  
+  // Criando um paciente
+  Patient *patient = create_patient(1, nome_paciente, &birthdate);
 
-    // my_strcpy
-    my_strcpy(str1, str2);
-    printf("Copied string: %s\n", str1);
+  // Imprimindo informações do paciente criado
 
-    // my_strcat
-    char str3[50] = "Hello, ";
-    my_strcat(str3, str2);
-    printf("Concatenated string: %s\n", str3);
+  char* day_string =  get_birthday_weekday(ano, mes, dia, patient);    
+  printf("Paciente criado:\n");
+  printf("ID: %d\n", get_patient_id(patient));
+  printf("Nome: %s\n", get_patient_name(patient));
+  printf("Data de Nascimento: %s %02d/%02d/%04d\n",day_string, dia, mes, ano);
 
-    // my_strcmp
-    int result = my_strcmp("apple", "banana");
-    if (result == 0)
-        printf("Strings are equal\n");
-    else if (result < 0)
-        printf("str1 is less than str2\n");
-    else
-        printf("str1 is greater than str2\n");
+  // Definindo uma data e hora fictícias para o exame
+  time_t current_time;
 
-    return 0;
+  struct tm *exam_time;
+
+  // Tempo do exame éo tempo atual
+  current_time = time(NULL); // Obter o tempo atual em segundos desde 01/01/1970
+  exam_time = localtime(&current_time); // Converter o tempo para a hora local
+
+  //Criando um exame associado ao paciente criado
+  Exam *exam = create_exam(101, get_patient_id(patient), 1, exam_time);
+
+  // Imprimindo informações do exame criado
+  printf("\nExame criado:\n");
+  printf("ID: %d\n", get_exam_id(exam));
+  printf("ID do Paciente: %d\n", get_exam_patient_id(exam));
+  printf("ID do Aparelho de Raio-X: %d\n", get_exam_rx_id(exam));
+  printf("Data e Hora do Exame: %s\n", asctime(get_exam_time(exam)));
+
+  // Liberando a memória alocada
+  destroy_exam(exam);
+  destroy_patient(patient);
+  return 0;
 }
