f1273aa1004f1212ac05e74dfcd16c6dcb7269ba
alliciarocha <155778524+alliciarocha@users.noreply.github.com>
2024-07-08 16:53:25 -0300

    Update patient.h

--- patient.h
+++ patient.h
@@ -1,1 +1,16 @@
+// Cabeçalhos das funções e estruturas desenvolvidas
+#ifndef PATIENT_H
+#define PATIENT_H
+#include <time.h>
 
+// Tipos de dados abstratos
+typedef struct patient Patient;
+
+// Funções relacionadas ao paciente
+Patient *create_patient(int, const char *, struct tm *birthdate);
+void destroy_patient(Patient *patient);
+int get_patient_id(Patient *patient);
+const char *get_patient_name(Patient *patient);
+struct tm *get_patient_birthdate(Patient *patient);
+
+#endif
