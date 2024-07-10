c3fca80e9cb514658d06ea46adc062089d697bab
alliciarocha <155778524+alliciarocha@users.noreply.github.com>
2024-07-10 19:57:26 -0300

    Update patient.c

--- patient.c
+++ patient.c
@@ -1,98 +1,98 @@
-#include "patient.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
+  #include "patient.h"
+  #include <stdio.h>
+  #include <stdlib.h>
+  #include <string.h>
+  #include <time.h>
 
-// TAD Patient
-struct patient {
-  int id;
-  char *name;
-  struct tm *birthdate;
-};
+  // TAD Patient
+  struct patient {
+    int id;
+    char *name;
+    struct tm *birthdate;
+  };
 
-// FUNÇÕES RELACIOINADAS AO PACIENTE
-// Cria paciente
-Patient *create_patient(int id, const char *name, struct tm *birthdate) {
-  // Verifica se name e birthdate são nulos
-  if (name == NULL) {
-    printf("Erro: o ponteiro name não aponta para nenhum endereço de memória "
-           "corretamente\n");
-    return NULL; // Retorna NULL caso o ponteiro não aponta para nenhum endereço
-                 // de memória corretamente
-  } else if (birthdate == NULL) {
-    printf("Erro: o ponteiro name não aponta para nenhum endereço de memória "
-           "corretamente\n");
-    return NULL; // Retorna NULL caso o ponteiro não aponta para nenhum endereço
-                 // de memória corretamente
+  // FUNÇÕES RELACIOINADAS AO PACIENTE
+  // Cria paciente
+  Patient *create_patient(int id, const char *name, struct tm *birthdate) {
+    // Verifica se name e birthdate são nulos
+    if (name == NULL) {
+      printf("Erro: o ponteiro name não aponta para nenhum endereço de memória "
+             "corretamente\n");
+      return NULL; // Retorna NULL caso o ponteiro não aponta para nenhum endereço
+                   // de memória corretamente
+    } else if (birthdate == NULL) {
+      printf("Erro: o ponteiro name não aponta para nenhum endereço de memória "
+             "corretamente\n");
+      return NULL; // Retorna NULL caso o ponteiro não aponta para nenhum endereço
+                   // de memória corretamente
+    }
+
+    // Alocar memória para a estrutura Patient e verifica se a memória foi alocada corretamente
+    Patient *patient = malloc(sizeof(struct patient));
+
+    if (patient == NULL) {
+      printf("Erro ao alocar memória\n");
+      return NULL; // Retorna NULL caso a memória não foi alocada corretamente
+    }
+
+    // Atribui o ID do paciente
+    patient->id = id;
+    
+    // Aloca memória para o nome do paciente e verifica se a memória foi alocada
+    // corretamente
+    patient->name = (char *)malloc(sizeof(char) * strlen(name) + 1);
+
+    if (patient->name == NULL) {
+      printf("Erro ao alocar memória\n");
+      destroy_patient(patient); // Liberar a memória
+      return NULL; // Retorna NULL caso a memória não foi alocada corretamente
+    }
+
+    // Copia o nome do paciente para a atribuição do nome do paciente
+    strcpy(patient->name, name); 
+
+    // Copia a estrutura tm para a atribuição da data de nascimento do paciente
+    patient->birthdate = birthdate;
+
+    return patient;
   }
 
-  // Alocar memória para a estrutura Patient e verifica se a memória foi alocada corretamente
-  Patient *patient = malloc(sizeof(struct patient));
-
-  if (patient == NULL) {
-    printf("Erro ao alocar memória\n");
-    return NULL; // Retorna NULL caso a memória não foi alocada corretamente
+  // função para destruir a estrutura alocada para o paciente
+  void destroy_patient(Patient *patient) {
+    if (patient != NULL) {
+      free(patient); // Liberar mémoria alocada para estrutura Patient
+    }
   }
 
-  // Atribui o ID do paciente
-  patient->id = id;
+  // função para retornar ID do paciente
+  int get_patient_id(Patient *patient) {
+    // Verificar se o ponteiro patient é NULL
+    if (patient == NULL) {
+      printf("Erro: o ponteiro é NULL\n");
+      return NULL; // Retorna NULL caso o ponteiro não aponte para nenhum endereço de memória corretamente
+    }
 
-  // Aloca memória para o nome do paciente e verifica se a memória foi alocada
-  // corretamente
-  patient->name = (char *)malloc(sizeof(char) * (strlen(name) + 1));
-
-  if (patient->name == NULL) {
-    printf("Erro ao alocar memória\n");
-    destroy_patient(patient); // Liberar a memória
-    return NULL; // Retorna NULL caso a memória não foi alocada corretamente
+    return patient->id;
   }
 
-  // Copia o nome do paciente para a atribuição do nome do paciente
-  strcpy(patient->name, name);
+  // função para retornar o nome do paciente
+  const char *get_patient_name(Patient *patient) {
+    // Verificar se o ponteiro patient é NULL
+    if (patient == NULL) {
+      printf("Erro: o ponteiro é NULL\n");
+      return NULL; // Retorna NULL caso o ponteiro não aponte para nenhum endereço de memória corretamente
+    }
 
-  // Copia a estrutura tm para a atribuição da data de nascimento do paciente
-  memcpy(patient->birthdate, birthdate, sizeof(struct tm));
-
-  return patient;
-}
-
-// função para destruir a estrutura alocada para o paciente
-void destroy_patient(Patient *patient) {
-  if (patient != NULL) {
-    free(patient); // Liberar mémoria alocada para estrutura Patient
+    return patient->name; 
   }
-}
 
-// função para retornar ID do paciente
-int get_patient_id(Patient *patient) {
-  // Verificar se o ponteiro patient é NULL
-  if (patient == NULL) {
-    printf("Erro: o ponteiro é NULL\n");
-    return NULL; // Retorna NULL caso o ponteiro não aponte para nenhum endereço de memória corretamente
-  }
-  
-  return patient->id;
-}
+  // estrutura para retornar data de nascimento
+  struct tm *get_patient_birthdate(Patient *patient) { 
+    // Verificar se o ponteiro patient é NULL
+    if (patient == NULL) {
+      printf("Erro: o ponteiro é NULL\n");
+      return NULL; // Retorna NULL caso o ponteiro não aponte para nenhum endereço de memória corretamente
+    }
 
-// função para retornar o nome do paciente
-const char *get_patient_name(Patient *patient) {
-  // Verificar se o ponteiro patient é NULL
-  if (patient == NULL) {
-    printf("Erro: o ponteiro é NULL\n");
-    return NULL; // Retorna NULL caso o ponteiro não aponte para nenhum endereço de memória corretamente
+    return patient->birthdate;
   }
-  
-  return patient->name; 
-}
-
-// estrutura para retornar data de nascimento
-struct tm *get_patient_birthdate(Patient *patient) { 
-  // Verificar se o ponteiro patient é NULL
-  if (patient == NULL) {
-    printf("Erro: o ponteiro é NULL\n");
-    return NULL; // Retorna NULL caso o ponteiro não aponte para nenhum endereço de memória corretamente
-  }
-  
-  return patient->birthdate;
-}
