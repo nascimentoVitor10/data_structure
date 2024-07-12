9561a4830eed3421d8ddeeae5127f01fcfaae1ad
alliciarocha <155778524+alliciarocha@users.noreply.github.com>
2024-07-12 02:53:53 -0300

    Update exam.c

--- exam.c
+++ exam.c
@@ -16,17 +16,26 @@ struct exam{
 
 // cria o exame
 Exam* create_exam(int id, int patient_id, int rx_id, struct tm *time){
-  Exam* exam = malloc(sizeof(struct exam)); // Alocar memória para a estrutura Exam
+  // verifica se time é nulo
+  if (time == NULL) {
+    printf("Erro: o ponteiro name não aponta para nenhum endereço de memória corretamente\n");
+    return NULL;
+  }
+
+  // aloca memória para a estrutura Exam
+  Exam* exam = malloc(sizeof(struct exam)); 
   // Verifica se a memória foi alocada corretamente
   if (exam == NULL){
     printf("Erro ao alocar memória!\n");
     exit(1); // Abortar programa caso a memória não foi alocada corretamente
   }
+  
   exam->id = id;
   exam->patient_id = patient_id;
   exam->rx_id = rx_id;
-  exam->time = time; 
-  return exam; // Returning the pointer to the allocated memory
+  exam->time = time;
+  
+  return exam;
 }
 
 // função para destruir a estrutura alocada para o exame
@@ -38,20 +47,40 @@ void destroy_exam(Exam *exam){
 
 // função para retornar ID do exame
 int get_exam_id(Exam *exam){
+  // Verificar se o ponteiro exam é NULL
+  if (exam == NULL) {
+    printf("Erro: o ponteiro é NULL\n");
+    return NULL; // Retorna NULL caso o ponteiro não aponte para nenhum endereço de memória corretamente
+  }
   return exam->id;
 }
 
 // função para retornar ID do paciente do exame
 int get_exam_patient_id(Exam *exam){
+  // Verificar se o ponteiro exam é NULL
+  if (exam == NULL) {
+    printf("Erro: o ponteiro é NULL\n");
+    return NULL; // Retorna NULL caso o ponteiro não aponte para nenhum endereço de memória corretamente
+  }
   return exam->patient_id;
 }
 
 // função para retornar ID do aparelho do exame
 int get_exam_rx_id(Exam *exam){
+  // Verificar se o ponteiro exam é NULL
+  if (exam == NULL) {
+    printf("Erro: o ponteiro é NULL\n");
+    return NULL; // Retorna NULL caso o ponteiro não aponte para nenhum endereço de memória corretamente
+  }
   return (*exam).rx_id; 
 }
 
 // estrutura para retornar data e hora do exame
 struct tm* get_exam_time(Exam *exam){
+  // Verificar se o ponteiro exam é NULL
+  if (exam == NULL) {
+    printf("Erro: o ponteiro é NULL\n");
+    return NULL; // Retorna NULL caso o ponteiro não aponte para nenhum endereço de memória corretamente
+  }
   return exam->time; 
 }
